f51afc5cf7f4063d841fa39da74eea0b83bb13b1
Szymon Cieśla <cszymon01@gmail.com>
2023-01-16 10:29:36 +0100

    new: created room-categories section

---
+++ blocks/lazyblock-roomcategories/block.php
@@ -0,0 +1,21 @@
+<section class="products section-padding">
+            <div class="container">
+                <div class="products__boxes">
+                    <div class="products__box">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/Łózko-dreniane-aranż_29064_maly-RGB.jpeg" alt="" class="products__img">
+                        <p class="products__text">Salon</p>
+                        <a href="product-category/meble/salon/" class="products__btn">Sprawdź</a>
+                    </div>
+                    <div class="products__box">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/Łózko-dreniane-aranż_29064_maly-RGB.jpeg" alt="" class="products__img">
+                        <p class="products__text">Sypialnia</p>
+                        <a href="product-category/meble/sypialnia/" class="products__btn">Sprawdź</a>
+                    </div>
+                    <div class="products__box">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/Łózko-dreniane-aranż_29064_maly-RGB.jpeg" alt="" class="products__img">
+                        <p class="products__text">Dodatki</p>
+                        <a href="product-category/meble/dodatki/" class="products__btn">Sprawdź</a>
+                    </div>
+                </div>
+            </div>
+        </section>
