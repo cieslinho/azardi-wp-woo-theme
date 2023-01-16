d59608939292ac4290ae936548bb7a3a518c46db
Szymon Cieśla <cszymon01@gmail.com>
2023-01-16 10:18:56 +0100

    new: added categories section

---
+++ blocks/lazyblock-categories/block.php
@@ -0,0 +1,31 @@
+<section class="categories section-padding">
+            <div class="container">
+                <h2 class="section-title">Kategorie</h2>
+                <div class="categories__boxes">
+                    <a href="/product-category/lozka/" class="categories__box">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/AR_carmen_2313_low_res.jpeg" alt="" class="categories__img">
+                        <div class="categories__bottom">
+                            <h3 class="categories__title">Łóżka</h3>
+                        </div>
+                    </a>
+                    <a href="/product-category/meble/" class="categories__box">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/AR_carmen_2313_low_res.jpeg" alt="" class="categories__img">
+                        <div class="categories__bottom">
+                            <h3 class="categories__title">Meble</h3>
+                        </div>
+                    </a>
+                    <a href="/product-category/materace/" class="categories__box">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/AR_carmen_2313_low_res.jpeg" alt="" class="categories__img">
+                        <div class="categories__bottom">
+                            <h3 class="categories__title">Materace</h3>
+                        </div>
+                    </a>
+                    <a href="/product-category/tkaniny/" class="categories__box">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/AR_carmen_2313_low_res.jpeg" alt="" class="categories__img">
+                        <div class="categories__bottom">
+                            <h3 class="categories__title">Tkaniny</h3>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </section>
