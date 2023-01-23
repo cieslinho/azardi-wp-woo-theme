af360a41b245e0ebfd0ec33d7bb4bc739e335812
Szymon Cieśla <cszymon01@gmail.com>
2023-01-23 16:46:45 +0100

    new: styled returns page

---
+++ blocks/lazyblock-returns/block.php
@@ -0,0 +1,31 @@
+<section class="returns">
+    <div class="container">
+        <h2 class="section-title">Zwroty i reklamacje</h2>
+        <div class="returns__boxes">
+            <div class="returns__box">
+                <h3 class="returns__box-title">Zwroty</h3>
+                <p class="returns__box-text">Każdy produkt firmy Azardi produkowany jest na zamówienie, nie przyjmujemy zwrotu zamówionego towaru, ponieważ przedmiot jest rzeczą nieprefabrykowaną, wyprodukowaną według specyfikacji konsumenta lub służąca zaspokojeniu jego zindywidualizowanych potrzeb (rozdz. 8 regulaminu pkt. 4 akapit 3).</p>
+            </div>
+            <div class="returns__box">
+                <h3 class="returns__box-title">Reklamacje</h3>
+                <p class="returns__box-text">Reklamacje prosimy kierować w formie pisemnej na adres:</p>
+                <p class="returns__box-text">Azardi S.C.</p>
+                <p class="returns__box-text">Mroczeń 93, 63-604 Baranów</p>
+                <p class="returns__box-text">lub adres e-mail: <a href="mailto:biuro@azardi.art">biuro@azardi.art</a></p>
+            </div>
+            <ul class="returns__box returns__list">
+                <li class="returns__box-text returns__list-item">Mogą Państwo skorzystać z <a href="<?php echo get_template_directory_uri(); ?>/assets/files/reklamacja.pdf">wzoru reklamacji</a>, ale nie jest to warunkiem rozpatrzenia reklamacji.</li>
+                <li class="returns__box-text returns__list-item">W przypadku stwierdzenia, że przesyłka z Towarem jest naruszona, doznała ubytku lub uszkodzenia, prosimy niezwłocznie (nie później niż w ciągu 7 dni od dnia odbioru przesyłki) poinformować nas o tym. Takie działanie umożliwi dochodzenie roszczeń od przewoźnika. Nie jest to warunkiem rozpatrzenia reklamacji.</li>
+                <li class="returns__box-text returns__list-item">Reklamacja powinna zawierać <b>szczegółowy opis problemu</b> i <b>żądanie Klienta</b>, ewentualnie także <b>dokumentację fotograficzną</b>.</li>
+                <li class="returns__box-text returns__list-item">Reklamacja zostanie rozpatrzona w terminie <b>30 dni</b>.</li>
+                <li class="returns__box-text returns__list-item">Szczegółowy opis prawny można przeczytać w <a href="/polityka-prywatnosci">Polityce poprawności</a>.</li>
+            </ul>
+            <div class="returns__box">
+                <h3 class="returns__box-title">Gwarancja</h3>
+                <ul class="returns__list">
+                    <li class="returns__box-text returns__list-item">Na wszystkie produkty w naszym sklepie udzielamy <b>2 letniej gwarancji</b>.</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
