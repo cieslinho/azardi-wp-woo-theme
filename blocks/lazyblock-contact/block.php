90611e93454217413e1409f04b29300dd4d1f191
Szymon Cieśla <cszymon01@gmail.com>
2023-01-24 15:52:37 +0100

    new: styled contact page

---
+++ blocks/lazyblock-contact/block.php
@@ -0,0 +1,57 @@
+<section class="contact">
+    <div class="container">
+        <h2 class="section-title">Kontakt</h2>
+        <div class="contact__boxes">
+<div class="contact__box contact__box-left">
+<div class="contact__box">
+                <div class="contact__box-icons">
+                <i class='bx bxs-phone'></i>
+                <p class="contact__box-text">Infolinia</p>
+                </div>
+                <div class="contact__box-middle">
+                    <a class="contact__box-btn"  href="tel:+48 884 822 444">+48 884 822 444</a>
+                </div>
+                <div class="contact__box-bottom">
+                    <p class="contact__box-text">pn-sb:  8:00 – 16:00</p>
+                </div>
+            </div>
+            <div class="contact__box">
+                <div class="contact__box-icons">
+                <i class='bx bxs-envelope'></i>
+                <p class="contact__box-text">Wiadomość</p>
+                </div>
+                <div class="contact__box-middle">
+                <p class="contact__box-text">Wolisz do nas napisać? Nie ma problemu! Wyślij swoje zapytanie na adres:</p>
+                </div>
+                <div class="contact__box-bottom">
+                <a  class="contact__box-btn" href="mailto:biuro@azardi.pl">biuro@azardi.pl</a>
+                <p class="contact__box-text">
+                Lub wypełnij formularz poniżej. Nasi eksperci odpowiedzą na twoją wiadomość w możliwie najszybszym czasie.
+                </p>
+                </div>
+            </div>
+</div>
+            <div class="contact__box contact__box-right">
+                <form action="" method="post" class="contact__form">
+                    <div class="contact__form-top">
+                    <label for="name" class="contact__form-label">Imię i nazwisko:</label>
+                    <input type="text" class="contact__form-input" id="name">
+                    <label for="email" class="contact__form-label">Adres e-mail:</label>
+                    <input type="email" class="contact__form-input" id="email">
+                    </div>
+                    <div class="contact__form-middle">
+                    <label for="mobile" class="contact__form-label">Telefon:</label>
+                    <input type="tel" class="contact__form-input" id="mobile">
+                    <label for="subject" class="contact__form-label">Temat:</label>
+                    <input type="text" class="contact__form-input" id="subject">
+                    </div>
+                    <div class="contact__form-bottom">
+                    <label for="message" class="contact__form-label">Treść wiadomości:</label>
+                    <textarea name="message" id="message" cols="30" rows="10" class="contact__form-textarea"></textarea>
+                    <button class="contact__form-btn" type="submit">wyślij</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
