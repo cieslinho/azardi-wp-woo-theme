e97de537f89218e8880d618e7ff2834586519dae
Szymon Cieśla <cszymon01@gmail.com>
2023-01-27 19:09:39 +0100

    new: styled rules page

---
+++ blocks/lazyblock-rules/block.php
@@ -0,0 +1,91 @@
+<section class="rules">
+    <div class="container">
+        <h2 class="section-title">Regulamin</h2>
+        <div class="rules__boxes">
+            <ol class="rules__box rules__list">
+                <p class="rules__list-text">1. Ogólne warunki zakupów:</p>
+                <li class="rules__list-item">Strona „<a href="/">www.azardi.pl</a>” zwana dalej „Azardi sklep internetowy” jest własnością Azardi s.c., ul. Mroczeń 93, 63-604 Baranów, NIP 6192048542.</li>
+            <li class="rules__list-item">Azardi sklep internetowy prowadzi sprzedaż towarów za pośrednictwem sieci www. Dostawy towarów zamawianych za pośrednictwem „Azardi sklep internetowy” są realizowane tylko na terenie Rzeczpospolitej Polskiej. Stroną dokonującą zakupów w „Azardi sklep internetowy” mogą być: osoby fizyczne, osoby prawne lub jednostki organizacyjne nieposiadające osobowości prawnej, zwane w dalszej części niniejszego regulaminu „Klientem”.</li>
+            <li class="rules__list-item">Podane na stronach „Azardi sklep internetowy” ceny obowiązujące w dniu złożenia zamówienia obowiązują od dnia złożenia zamówienia do dnia jego realizacji.</li>
+            <li class="rules__list-item">Wszystkie ceny podawane są w złotych polskich i zawierają podatek VAT.</li>
+            <li class="rules__list-item">„Azardi sklep internetowy” oferuje zakupy w trzech trybach:</li>
+            <ul class="rules__sublist">
+                <li class="rules__sublist-item">Tryb „Zwykły”</li>
+                <li class="rules__sublist-item">Tryb „Szybki zakup”</li>
+                <li class="rules__sublist-item">Tryb „Przelewy24.pl”</li>
+            </ul>
+            <ul class="rules__sublist-second">
+                <li class="rules__sublist-item">Tryb „Zwykły” polega na zakupie towarów w drodze uprzedniej rejestracji w Azardi sklep internetowy oraz dokonania zapłaty za zamówione towary. Zapłaty można dokonać przelewem elektronicznym za pośrednictwem systemu płatności elektronicznych Przelewy24.pl. Podczas rejestracji Klient podaje dane osobowe takie jak imię i nazwisko, adres, nr telefonu, adres e-mail, hasło.</li>
+                <li class="rules__sublist-item">
+                Tryb „Szybki zakup” polega na zakupie towarów bez rejestrowania się w Azardi sklep internetowy, zapłaty można dokonać przelewem elektronicznym za pośrednictwem systemu płatności elektronicznych Przelewy24.pl. Klient podaje dane związane z adresem dostawy zawierające dane osobowe imię i nazwisko, adres, adres e-mail, nr telefonu.
+                </li>
+                <li class="rules__sublist-item">
+                Tryb „Konto Przelewy24.pl” – Klient po dokonaniu wyboru towarów zostaje przekierowany do systemu Przelewy24.pl, w którym dokonuje logowania/rejestracji i dokonuje płatności przelewem elektronicznym lub karta kredytową.
+                </li>
+            </ul>
+            <li class="rules__list-item">W każdym trybie, zamówienia na towary są składane przez Klientów na stronie internetowej „Azardi sklep internetowy” poprzez wypełnienie odpowiedniego formularza.
+</li>
+<li class="rules__list-item">Serwer „Azardi sklep internetowy” wysyła automatycznie wiadomość e-mail do Klienta w celu uzyskania potwierdzenia złożonego zamówienia przy płatności za pobraniem. Wiadomość, o której mowa w zdaniu poprzedzającym, będzie zawierać informacje na temat wysokości i kosztów przesyłki. 
+</li>
+<li class="rules__list-item">Procedura realizacji złożonego zamówienia rozpoczyna się w momencie, gdy klient potwierdzi e-mailem swoje zamówienie oraz zapłaci za towar za pomocą przelewu elektronicznego lub karty kredytowej w systemie płatności elektronicznych Przelewy24.pl.</li>
+<li class="rules__list-item">
+Jako datę zapłaty liczy się termin wpływu pieniędzy na konto „Azardi sklep internetowy. Zapłata musi się znaleźć na koncie „Azardi sklep internetowy” nie później niż po 7-miu dniach od daty złożenia zamówienia. Po bezskutecznym upływie 7-dniowego terminu na zapłatę odpowiednio: 30% zaliczki towaru i za dostawę (w przypadku wyboru płatności „za pobraniem”) oraz za towar (w przypadku wyboru pozostałych form płatności) zamówienie zostanie anulowane i nie będzie wiązać Azardi sklep internetowy. 
+</li>
+<li class="rules__list-item">Termin realizacji zamówienia to 30 dni roboczych od dnia spełnienia warunków zawartych w punkcie 9 niniejszego regulaminu, chyba że Klient życzy sobie ich dostarczenia w późniejszym terminie. Za termin realizacji zamówienia przyjmujemy czas w jakim produkcja wykonuje zamówienie, do tego terminu należy dodać czas dostawy towarów od 7-14 dni (jeśli klient zamówi meble z dostawą pod wskazany adres). W takim przypadku osoba z Biura Obsługi Klienta bezpośrednio kontaktuje się z Klientem w celu umówienia terminu dostawy. Termin dostawy mebli może być dłuższy od terminu umówionego z Klientem, z przyczyn leżących po stronie przewoźnika, jednak nie będzie dłuższy niż 14 dni o czym poinformujemy klienta wcześniej.</li>
+<li class="rules__list-item">
+W przypadku złożenia lub potwierdzenia przez Klienta złożonego zamówienia w dzień wolny od pracy, bieg wyżej określonego terminu rozpoczyna się w najbliższym dniu roboczym.
+</li>
+<li class="rules__list-item">
+Koszty dostaw znajdują się w zakładce „Dostawa”.
+</li>
+<li class="rules__list-item">
+Faktura VAT imienna lub na firmę będzie zawierać dane, które Klient poda podczas składania zamówienia i będzie przesyłana drogą mailową.
+</li>
+            </ol>
+            <ol class="rules__box rules__list">
+            <p class="rules__list-text">2. Przetwarzanie danych:</p>
+            <li class="rules__list-item">Zgodnie z ustawą z 25 grudnia 2014 roku o prawach konsumenta (Dz.U. 2014 poz. 827 z późn. zm.) prawo odstąpienia od umowy zawartej poza lokalem przedsiębiorstwa lub na odległość nie przysługuje konsumentowi w odniesieniu do umów:
+            </li>
+            <ul class="rules__sublist">
+            <li class="rules__sublist-item">O świadczenie usług, jeżeli przedsiębiorca wykonał w pełni usługę za wyraźną zgodą konsumenta, który został poinformowany przed rozpoczęciem świadczenia, że po spełnieniu świadczenia przez przedsiębiorcę utraci prawo odstąpienia od umowy;            </li>
+            <li class="rules__sublist-item">w której przedmiotem świadczenia jest rzecz nieprefabrykowana, wyprodukowana według specyfikacji konsumenta lub służąca zaspokojeniu jego zindywidualizowanych potrzeb.</li>
+            </ul>
+            </ol>
+            <ol class="rules__box rules__list">
+                <p class="rules__list-text">3. Reklamacje:</p>
+                <p class="rules__list-text">(dotyczą wyłącznie Klientów będących konsumentami, w rozumieniu art. 221 Kodeksu cywilnego)</p>
+                <p class="rules__list-text">Towar przed wysłaniem do Klienta podlega dokładnemu sprawdzeniu pod względem jakościowym.</p>
+                <li class="rules__list-item">Reklamacje rozpatrywane będą na podstawie przesłanego za pośrednictwem Internetu wypełnionego formularza reklamacji znajdującego się na stronie <a href="/">azardi.pl</a> lub na podstawie pisemnego oświadczenia Klienta. Reklamacje rozpatrywane będą w ciągu 14 dni od daty otrzymania zgłoszenia reklamacji przez <b>„Azardi sklep internetowy”</b>.
+                </li>
+                <li class="rules__list-item">W przypadku uzasadnionej reklamacji, wadliwy towar zostanie naprawiony lub wymieniony na nowy, pod warunkiem, że wada ma charakter istotny.</li>
+                <li class="rules__list-item">Reklamacje zgłaszane w formie pisemnej należy przesłać pocztą na adres:  <b>Dział obsługi klienta Azardi ul. Mroczeń 93, 63-604 Baranów</b> lub drogą mailową na adres <a href="mailto:biuro@azardi.pl">biuro@azardi.pl</a></li>
+                <li class="rules__list-item">W pisemnym, podpisanym przez reklamującego, oświadczeniu należy podać:</li>
+                <ul class="rules__sublist">
+                    <li class="rules__sublist-item">Imię i nazwisko</li>
+                    <li class="rules__sublist-item">Datę zakupu oraz numer zamówienia</li>
+                    <li class="rules__sublist-item">Dokładny adres (ulica, numer domu, kod, miejscowość)</li>
+                    <li class="rules__sublist-item">Nazwę, numery, ilość towarów, które są reklamowane</li>
+                    <li class="rules__sublist-item">Powód reklamacji</li>
+                </ul>
+                <li class="rules__list-item">
+                Dział Reklamacji maksymalnie w ciągu 14 dni od dnia otrzymania reklamacji rozpatrzy zasadność reklamacji. Informacja o przyjęciu lub odrzuceniu reklamacji zostanie przesłana Klientowi (będącemu konsumentem) na adres podany w formularzu reklamacyjnym. Jeśli reklamacja zostanie uznana pracownik Działu Reklamacji skontaktuje się z Klientem w celu ustalenia terminu i trybu likwidacji szkody.
+                </li>
+                <li class="rules__list-item">
+                Wszelkie wątpliwości związane z reklamacjami można wyjaśniać pod adresem e-mail <a href="mailto:biuro@azardi.pl">biuro@azardi.pl</a> lub pod numerem telefonu <a href="tel:+48884822444">+48 884 822 444</a>.</li>
+            </ol>
+            <ol class="rules__box rules__list">
+                <p class="rules__list-text">4. Ochrona danych osobowych.</p>
+                <li class="rules__list-item">Zgodnie z ustawą z dnia 29 sierpnia 1997 roku o ochronie danych osobowych (tekst jednolity Dz. U. Nr 101 z 2002 r. poz. 926, z późn. zm.) właściciel Sklepu Internetowego azardi.pl informuje, że dane osobowe Klientów są przechowywane w zbiorze danych Azardi z siedzibą w Baranowie, przy ul. Mroczeń 93.</li>
+                <li class="rules__list-item">Dane osobowe będą zbierane i przetwarzane wyłącznie na potrzeby realizacji zamówień składanych przez Klienta w Sklepie Internetowym <a href="/">azardi.pl</a>.</li>
+                <li class="rules__list-item">Każdy Klient ma prawo wglądu do swoich danych, poprawienia, żądania zaprzestania przetwarzania oraz usunięcia danych oraz ma prawo sprzeciwu, który należy zgłosić do siedziby Azardi.</li>
+                <li class="rules__list-item">Firma Azardi zapewnia, że wszystkie informacje i dane przekazywane przez Klienta za pośrednictwem strony internetowej azardi.pl pozostaną poufne i w odpowiedni sposób zabezpieczone.</li>
+                <li class="rules__list-item">Złożenie zamówienia w „Azardi sklep internetowy” oznacza zaakceptowanie postanowień niniejszego regulaminu w brzmieniu obowiązującym w dniu złożenia przez Klienta zamówienia.</li>
+                <li class="rules__list-item">Utrwalenie, zabezpieczenie i udostępnienie istotnych postanowień zawieranej umowy następuje przez wydrukowanie i przekazanie Klientowi wraz z przesyłką faktury VAT.</li>
+                <li class="rules__list-item">W razie wątpliwości poczytuje się, że umowa sprzedaży zawierana jest między Klientem, a właścicielem „Azardi sklep internetowy” z chwilą potwierdzenia przez Klienta zamówienia, o którym mowa w punkcie I ust. 8-11 niniejszego Regulaminu.</li>
+                <li class="rules__list-item">Prawem właściwym dla umowy sprzedaży towarów pomiędzy Azardi sklep internetowy, a Klientem jest prawo polskie, w szczególności przepisy Ustawy z dnia 2 marca 2000r o ochronie niektórych praw konsumentów oraz odpowiedzialności za szkodę wyrządzoną przez produkt niebezpieczny (Dz.U. 2000 r., Nr 22, poz. 271 z późn. zmianami) oraz ustawa z dnia 30 maja 2014r. o prawach konsumenta (Dz.U. 2014 r. poz. 827).</li>
+                <li class="rules__list-item">Sądem właściwym dla rozpatrywania sporów wynikających z umowy sprzedaży jest sąd właściwy według zasad ogólnych.</li>
+                <li class="rules__list-item">Niniejszy Regulamin obowiązuje od dnia 01 luty 2023 roku.</li>
+            </ol>
+        </div>
+    </div>
+</section>
