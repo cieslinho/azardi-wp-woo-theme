24e951d6eff88a5f00ffb247b693f918bfa896e0
Szymon Cieśla <cszymon01@gmail.com>
2023-02-06 04:00:22 +0100

    fix: deleted nav-item payment-methods

--- inc/wc-modifications.php
+++ inc/wc-modifications.php
@@ -8,31 +8,6 @@ function azardi_shop_section_close(){
   echo '</div></div>';
 }
 
-
-function my_account_menu_order($menu_items){
- 
-  $reordered_items = array(
-      'dashboard' => __('Start', 'woocommerce'),
-      'edit-address' => __('Adresy', 'woocommerce'),
-      'edit-account' => __('Edytuj dane konta', 'woocommerce'),
-      'orders' => __('Orders', 'woocommerce'),
-      'payment-methods' => __('Metody Płatności', 'woocommerce'),
-      'customer-logout' => __('Logout', 'woocommerce')
-  );
-
-  return $reordered_items;
-}
-
-add_filter('woocommerce_account_menu_items', 'my_account_menu_order');
-
-function my_account_rename_items($menu_items){
-  $menu_items['payment-methods'] = __('Metody płatności', 'my_text_domain');
-  return $menu_items;
-}
-
-add_filter('woocommerce_account_menu_items', 'my_account_rename_items');
-
-
 remove_action('woocommerce_sidebar','woocommerce_get_sidebar');
 add_action('woocommerce_shop_loop_item_title', 'the_excerpt', 1);
 remove_action('woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart');
