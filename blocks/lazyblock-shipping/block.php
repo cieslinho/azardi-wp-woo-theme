bb9073ee977834143a705ae740797af1d6ff068f
Szymon Cieśla <cszymon01@gmail.com>
2023-02-06 03:51:40 +0100

    fix: changed text of shipping lazy-block

--- blocks/lazyblock-shipping/block.php
+++ blocks/lazyblock-shipping/block.php
@@ -5,10 +5,9 @@
                 <p class="shipping__list-text">
                 Przygotowaliśmy dla Państwa 3 formy odbioru zamówionych towarów:
                 </p>
-                <li class="shipping__list-item">Dostawa bez wniesienia 0 zł – Całkowicie darmowa dostawa na terenie całego kraju.</li>
-                <li class="shipping__list-item">Dostawa z wniesieniem i montażem 199 zł – Pełen pakiet usług. Dwuosobowy zespół dostarcza meble pod podany adres. Po wniesieniu towar jest rozpakowany oraz montowany we wskazanym przez klienta miejscu.</li>
-                <p class="shipping__list-text">Pakiet w cenie 199 zł obejmuje wniesienie mebli do II piętra (pomijając windę). Wniesienie mebli powyżej II piętra (bez windy) to dodatkowy koszt 20 zł od piętra</p>
-                <li class="shipping__list-item">Odbiór własny w magazynie – Odbiory własne mogą być realizowane od poniedziałku do piątku w godzinach 8-14. Adres: ul. Warszawska 28, 56-400 Spalice.</li>
+                <li class="shipping__list-item">Dostawa bez wniesienia - 150zł</li>
+                <li class="shipping__list-item">Dostawa z wniesieniem i montażem 199 zł – Pełen pakiet usług. Dwuosobowy zespół dostarcza meble pod podany adres. Po wniesieniu towar jest rozpakowany oraz montowany we wskazanym przez klienta miejscu. W celu wybrania tej opcji prosimy o kontakt telefoniczny.</li>
+                <li class="shipping__list-item">Odbiór własny w magazynie – Odbiory własne mogą być realizowane od poniedziałku do piątku w godzinach 8-14. Adres: ul. Mroczeń 93, 63-604 Baranów.</li>
                 <p class="shipping__list-text">Dostępne w 48h:</p>
                 <li class="shipping__list-item">Produkt dostępny w 2 dni robocze od momentu zamówienia. Odbierz go osobiście z naszego magazynu lub zamów z dostawą. Dostawa zamówienia: 7 do 14 dni roboczych.</li>
             </ul>
