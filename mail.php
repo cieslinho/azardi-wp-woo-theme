54be953ba700e120051c5e38e5e9cf5bc42f3392
Szymon Cieśla <cszymon01@gmail.com>
2023-02-10 16:12:16 +0100

    fix: sending email to new email address

--- mail.php
+++ mail.php
@@ -1,18 +1,17 @@
 <?php
-include('phpmailer/PHPMailerAutoload.php');
     //get data from form  
     $name = $_POST['name'];
     $email= $_POST['email'];
     $emailTo = $_POST['email-adress'];
+    $mobile = $_POST['mobile'];
     $message= $_POST['message'];
     $subject = $_POST['subject'];
-    $to = 'cszymon01@gmail.com';
-    $subject = "AZARDI - FORMULARZ KONTAKTOWY";
-    $txt = " Imię: ". $name . ' ' . $surname . "\r\n E-mail: " . $email . "\r\n Temat wiadomości: " . $subject . "\r\n Treść wiadomości: " . $message;
-    $headers = "Od: kontakt@azardi.pl";
+    $to = 'biuro@lozko24.pl';
+    $txt = " Imię: ". $name . "\r\n E-mail: " . $email . "\r\n Telefon: " .$mobile. "\r\n Temat wiadomości: " . $subject . "\r\n Treść wiadomości: " . $message;
+    $headers = "Od: no-reply@azardi.pl";
     if ($email != NULL) {
         mail($to,$subject,$txt,$headers);
     }
     //redirect
-    header("Location: http://azardishop.local/kontakt/wiadomosc-wyslana");
+    header("Location: /kontakt/wiadomosc-wyslana");
 ?>
