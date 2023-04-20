b8751b6e4713cf1e42f008ca87dd3b13d1ec256f
Szymon Cieśla <cszymon01@gmail.com>
2023-04-20 16:07:28 +0200

    added new features - changed pdf files

--- blocks/lazyblock-instructions/block.php
+++ blocks/lazyblock-instructions/block.php
@@ -2,22 +2,22 @@
     <div class="container">
         <h2 class="section-title">Instrukcje</h2>
             <div class="instructions__cards">
-                <a  href="<?php echo get_template_directory_uri(); ?>/assets/files/Instrukcja - składanie łóżka.pdf" target="_blank" class="instructions__card">
+                <a  href="<?php echo get_template_directory_uri(); ?>/assets/files/instrukcja-lozko.pdf" target="_blank" class="instructions__card">
                     <h4 class="instructions__card-title">Łóżko</h4>
                     <i class='bx bxs-file-pdf'></i>
                     <p class="instructions__card-subtitle"></p>
                 </a>
-                <a  href="<?php echo get_template_directory_uri(); ?>/assets/files/Instrukcja - składanie łóżka kontynentalnego.pdf" target="_blank" class="instructions__card">
+                <a  href="<?php echo get_template_directory_uri(); ?>/assets/files/instrukcja-kontynentalne.pdf" target="_blank" class="instructions__card">
                     <h4 class="instructions__card-title">Łóżko</h4>
                     <i class='bx bxs-file-pdf'></i>
                     <p class="instructions__card-subtitle">Kontynentalne</p>
                 </a>
-                <a  href="<?php echo get_template_directory_uri(); ?>/assets/files/Instrukcja - składanie pojemnika _zwykłego_.pdf" target="_blank" class="instructions__card">
+                <a  href="<?php echo get_template_directory_uri(); ?>/assets/files/instrukcja-pojemnik-zwykly.pdf" target="_blank" class="instructions__card">
                     <h4 class="instructions__card-title">Pojemnik</h4>
                     <i class='bx bxs-file-pdf'></i>
                     <p class="instructions__card-subtitle">Zwykły</p>
                 </a>
-                <a  href="<?php echo get_template_directory_uri(); ?>/assets/files/Instrukcja - składanie pojemnika _dwupozycyjnego_.pdf" target="_blank" class="instructions__card">
+                <a  href="<?php echo get_template_directory_uri(); ?>/assets/files/instrukcja-pojemnik-dwupozycyjny.pdf" target="_blank" class="instructions__card">
                     <h4 class="instructions__card-title">Pojemnik</h4>
                     <i class='bx bxs-file-pdf'></i>
                     <p class="instructions__card-subtitle">Dwupozycyjny</p>
